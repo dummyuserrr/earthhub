892afa5942bc26a91cb34e8dc6989cfed6c95d61
angryuserrr <dthrcrpz@gmail.com>
2017-04-30 16:36:14 +0800

    update

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -10,13 +10,14 @@
 			  <div class="panel-heading">
 					<div>
 			  		<h3><b>About Us</b></h3>
-			  	</div>
-				</div>
+			  		</div>
+			</div>
 			</div>
 				<div class="panel panel-default">
 				<div class="panel-heading">
 				<div class="panel-body" >
 			  	<div>
+			  		<h4 class="text-center alert alert-danger"><b>NOTE: </b>This application is still in BETA mode!</h4>
 			  		<h4><center><p style="font-family: Comic sans-serif"><i><b>
 			  			EarthHUB aims to help others to know what the problems are and what happens in our environment. EartHUB is here for discussions and exchange of ideas between all interested people about Earth, Energy, Man-made Disasters, Life, Pollution, Natural Phenomenons, and Chemicals.
 			  		</i></b></center></p></h4>
@@ -31,15 +32,15 @@
 			</div>
 				<div class="panel panel-default">
 				  <div class="panel-heading">
-						<div>
-				  		<h3><b>Developers</b></h3>
-							<div style="margin-left:65px">
-							<h5><b>Deither Corpuz</b></h5>
-							<h5><b>Jerome Valdez</b></h5>
-							<h5><b>Karl Cenise</b></h5>
-							<h5><b>Jeancy-Lyn Caan</b></h5>
+					<h3><b>Developers</b></h3>
+				<div class="panel-body">
+					<div style="margin-left:65px">
+						<h5><b>Deither Corpuz</b></h5>
+						<h5><b>Jerome Valdez</b></h5>
+						<h5><b>Karl Cenise</b></h5>
+						<h5><b>Jeancy-Lyn Caan</b></h5>
 				  	</div>
-					</div>
+				</div>
 				</div>
 			</div>
 		</div>
