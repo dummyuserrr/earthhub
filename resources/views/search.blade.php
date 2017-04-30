40fe7702020c72ecb87dc93759fe8124938ba52f
angryuserrr <dthrcrpz@gmail.com>
2017-04-30 19:03:17 +0800

    update

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -11,7 +11,13 @@
 			  <div class="panel-body">
 			  @foreach($posts as $post)
 			  	<div class="row">
-			  		<div class="col-sm-1 text-center"><h6>{{ $post->ratings }}<br>ratings</h6></div>
+			  		<div class="col-sm-1 text-center"><h6>
+			  			@if($post->ratings->where('useful', 1)->count() >= $post->ratings->where('useful', 0)->count())
+			  					{{ $post->ratings->where('useful', 1)->count() }}
+			  				@else
+			  					-{{ $post->ratings->where('useful', 0)->count() }}
+			  				@endif
+			  		<br>ratings</h6></div>
 			  		<div class="col-sm-1 text-center"><h6>{{ count($post->replies) }}<br>replies</h6></div>
 			  		<div class="col-sm-10"><a href="/post?p={{ $post->id }}"><p>{{ $post->title }}</p></a>
 			  		<p style="text-overflow: ellipsis; white-space: nowrap;overflow: hidden;">{{ $post->body }}</p>
